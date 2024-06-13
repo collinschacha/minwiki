79a859dfb6464cdb94b1fe89f1998cc85743a85e
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-06-13 14:35:31 +0300

    initial commit added a few styling features

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>MiniWiki</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
