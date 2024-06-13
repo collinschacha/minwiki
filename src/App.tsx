a7b13b9f55a19850e0a54bb593f087ffc43f3af6
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-06-13 15:17:11 +0300

    finished search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState();
+
+  const onChange = (e) => {
+    console.log(e.currentTarget.value);
+    setSearch(e.currentTarget.value);
+  };
+
+  const onSubmit = () => {
+    console.log("Searching for:", search);
+    fetch(`http://localhost:3000/api/search?q=${search}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Search results:", data);
+        setResults(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching search results:", error);
+      });
+  };
+
   return (
     <>
       <h1>MiniWiki</h1>
       <div className="search">
-        <input placeholder="Search..." type="text" />
-        <button type="submit">Go</button>
+        <input
+          placeholder="Search..."
+          type="text"
+          value={search}
+          onChange={onChange}
+        />
+        <button type="submit" onClick={onSubmit}>
+          Go
+        </button>
       </div>
     </>
   );
