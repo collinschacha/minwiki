2c1deb22f044f88f8b58f3f055ef5cc866ee50c4
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-06-13 14:43:03 +0300

    searchbar added

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ function App() {
   return (
     <>
       <h1>MiniWiki</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="search">
+        <input placeholder="Search..." type="text" />
+        <button type="submit">Go</button>
+      </div>
     </>
   );
 }
